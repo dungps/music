e403411f29ed1df69e55479dcae45ec360521bd6
Kevin Pham <Kevin Pham>
2017-03-26 00:42:18 +0700

    fix bug

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 module.exports = {
 	site: {
-		home: function() {
-			return process.env.NODE_ENV == 'production' ? 'http://music.dungps.com' : 'http://localhost:3000'
-		}
+		home: process.env.NODE_ENV == 'production' ? 'http://music.dungps.com' : 'http://localhost:3000'
 	}
 }
